9f7037d0a8eb534fce2d444d14a18e7a003477c8
xecua <courser4848@gmail.com>
2021-02-23 01:01:07 +0900

    add F5 VPN

---
+++ Casks/f5vpn.rb
@@ -0,0 +1,11 @@
+cask "f5vpn" do
+  version "latest"
+  sha256 "a5c0b03a3994818c66d9e7f0e38b8790e63007f379ecece2b0168cae049b0de3"
+
+  url "https://apm.nap.gsic.titech.ac.jp/public/download/mac_f5vpn.pkg"
+  name "f5vpn"
+  desc "F5 Networks VPN Client"
+  homepage ""
+
+  pkg "mac_f5vpn.pkg"
+end
